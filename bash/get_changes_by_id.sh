0dd1a3171351fb29a72862064ac8bcffaefa891d
Jesper Madsen <jesper.a.madsen@gmail.com>
2023-12-23 18:17:39 +0100

    sh: New format for get latest and get changes

---
+++ bash/get_changes_by_id.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Set required variables
+projectId="$1"
+apiKey="$2"
+deploymentId="$3"
+downloadFolder="$4"
+pipelineVendor="$5"
+
+# Not required, defaults to https://api.cloud.umbraco.com
+baseUrl="$6" 
+
+if [[ -z "$baseUrl" ]]; then
+    baseUrl="https://api.cloud.umbraco.com"
+fi
+
+### Endpoint docs
+# https://docs.umbraco.com/umbraco-cloud/set-up/project-settings/umbraco-cicd/umbracocloudapi#get-deployment-diff
+
+changeUrl="$baseUrl/v1/projects/$projectId/deployments/$deploymentId/diff"
+
+# Get diff - stores file as git-patch.diff
+function get_changes {
+  mkdir -p $downloadFolder # ensure folder exists
+  
+  responseCode=$(curl -s -w "%{http_code}" -L -o "$downloadFolder/git-patch.diff" -X GET $changeUrl \
+    -H "Umbraco-Cloud-Api-Key: $apiKey" \
+    -H "Content-Type: application/json")
+
+  if [[ 10#$responseCode -eq 204 ]]; then
+    echo "No Changes - You can continue"
+    remoteChanges = "no"
+    return
+  elif [[ 10#$responseCode -eq 200 ]]; then
+    echo "Changes detected"
+    $Response | Select-Object -ExpandProperty Content | Out-File "$DownloadFolder/git-patch.diff"
+    remoteChanges = "yes"
+    return
+  fi
+  echo "---Response Start---"
+  echo $Response
+  echo "---Response End---"
+  echo "Unexpected response - see above"
+  exit 1
+}
+
+if [[ -z "$latestDeploymentId" ]]; then
+  echo "I need a DeploymentId of an older deployment to download a git-patch"
+  exit 1
+fi
+
+get_changes
+
+## Write the latest deployment id to the pipelines variables for use in a later step
+if [[ "$pipelineVendor" == "GITHUB" ]]; then
+  echo "remoteChanges=$remoteChanges" >> "$GITHUB_OUTPUT"
+  exit 0
+elif [[ "$pipelineVendor" == "AZUREDEVOPS" ]]; then
+  echo "##vso[task.setvariable variable=remoteChanges;isOutput=true]$remoteChanges"
+  exit 0
+elif [[ "$pipelineVendor" == "TESTRUN" ]]; then
+  echo $pipelineVendor
+  exit 0
+fi
+
+echo "Please use one of the supported Pipeline Vendors or enhance script to fit your needs"
+echo "Currently supported are: GITHUB and AZUREDEVOPS"
+Exit 1
---
+++ bash/get_latest_deployment.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Set required variables
+projectId="$1"
+apiKey="$2"
+pipelineVendor="$3"
+
+# Not required, defaults to https://api.cloud.umbraco.com
+baseUrl="$4"
+
+if [[ -z $baseUrl ]]; then
+    baseUrl="https://api.cloud.umbraco.com"
+fi
+
+### Endpoint docs
+# https://docs.umbraco.com/umbraco-cloud/set-up/project-settings/umbraco-cicd/umbracocloudapi#get-deployments
+#
+# We want the Id of the latest deployment that created changes to cloud 
+# Filter deployments
+skip=0
+take=1
+# Exclude cloud null deployments
+includeNullDeployments=false
+
+queryString="take=$take&skip=$skip&includenulldeployments=$includeNullDeployments"
+
+url="$baseUrl/v1/projects/$projectId/deployments?$queryString"
+
+function call_api {
+  response=$(curl -s -w "%{http_code}" -X GET $url \
+    -H "Umbraco-Cloud-Api-Key: $apiKey" \
+    -H "Content-Type: application/json")
+    responseCode=${response: -3}  
+    content=${response%???}
+
+  if [[ 10#$responseCode -eq 200 ]]; then
+    latestDeploymentId=$(echo $content | jq -Rnr '[inputs] | join("\\n") | fromjson | .deployments[0].deploymentId')
+
+    if [[ -z $latestDeploymentId ]]; then
+        echo "No latest CICD Flow Deployments found"
+        echo "----------------------------------------------------------------------------------------"
+        echo "This is usually because you have yet to make changes to cloud through the CICD endpoints"
+    else 
+        echo "Latest CICD Flow Deployment:"
+        echo "$latestDeploymentId"
+    fi
+
+    return
+  fi
+  ## Let errors bubble forward 
+  echo "Unexpected API Response Code: $responseCode"
+  echo "---Response Start---"
+  echo $response
+  echo "---Response End---"
+
+  exit 1
+}
+
+call_api
+
+## Write the latest deployment id to the pipelines variables for use in a later step
+if [[ "$pipelineVendor" == "GITHUB" ]]; then
+  echo "latestDeploymentId=$deployment_id" >> "$GITHUB_OUTPUT"
+  exit 0
+elif [[ "$pipelineVendor" == "AZUREDEVOPS" ]]; then
+  echo "##vso[task.setvariable variable=latestDeploymentId;isOutput=true]$deployment_id"
+  exit 0
+elif [[ "$pipelineVendor" == "TESTRUN" ]]; then
+  echo $pipelineVendor
+  exit 0
+fi
+
+echo "Please use one of the supported Pipeline Vendors or enhance script to fit your needs"
+echo "Currently supported are: GITHUB and AZUREDEVOPS"
+Exit 1
